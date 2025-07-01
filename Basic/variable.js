3e5580153969f8875481e6645773c02420b04e2c
mudasir <mudasirahmad32@gmail.com>
2025-07-01 07:47:13 +0530

    1

---
+++ Basic/variable.js
@@ -0,0 +1,26 @@
+const mudasir=34;
+console.log(mudasir);
+let muzamil=67;
+console.log(muzamil);
+
+// mudasir=3; //constant cannot be changed 
+muzamil=56;
+
+console.log(muzamil); //here it got changed
+
+const aabid="Muzamil";
+let email="Mudasir@gmail.com";
+let city="Canada";
+console.table([aabid,email,city]); //it will show us in the table.
+// var mostly not used in new javacript beacuse of scope problem 
+
+//  "Use strict" treat all code as newer version of javascript.
+// https://tc39.es/ecma262/   documentation
+
+// use strict // means use newer version of javascript
+//  symbol // unique value for the javascript.
+// undefined // not yet defined 
+// null //standalone value.
+
+
+
