b3a3751f2253fa7ea26fde2257c0ba6106998e83
mudasir <mudasirahmad32@gmail.com>
2025-07-02 18:53:30 +0530

    My object

---
+++ Basic/object.js
@@ -0,0 +1,8 @@
+const mudasir={
+    name:"Mudasir",
+    roll:23,
+    class:"second"
+}
+console.log(mudasir.roll);
+const {roll}=mudasir
+console.log(roll);
---
+++ Basic/typeof.js
@@ -0,0 +1,18 @@
+let num1= new Number(100);
+console.log(typeof num1);  //object 
+
+console.log(num1.toString());
+
+
+let num=100;
+console.log(typeof num); // number 
+
+let charater= new String("Muzamil");
+console.log(charater);
+
+let bol= new Boolean(1);
+console.log(typeof bol);
+
+ 
+
+
