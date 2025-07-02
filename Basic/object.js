6bdde44c40625461c27b6c28edde736297e9c6b5
mudasir <mudasirahmad32@gmail.com>
2025-07-02 19:21:34 +0530

    My object

--- Basic/object.js
+++ Basic/object.js
@@ -1,3 +1,18 @@
+const obj1={1:"A",2:"B",3:"C"};
+const obj2={3:"D",4:"E"};
+const obj3={obj1,obj2};
+console.log(obj3);
+const obj4=Object.assign(obj1,obj2);
+console.log(obj4);
+
+const obj5={...obj1,...obj2};
+console.log(obj5);
+
+
+
+
+
+//object destructuring
 const mudasir={
     name:"Mudasir",
     roll:23,
