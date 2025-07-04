78b9f6721970b16ebe4d8d4c044cc74ee26a657d
mudasir <mudasirahmad32@gmail.com>
2025-07-04 16:04:56 +0530

    function

---
+++ Basic/Math.js
@@ -0,0 +1,30 @@
+// // const num = 499;
+// // console.log(num);
+// // const num1= new Number(99);
+// // console.log(typeof(num1));
+// // // console.log(num1.toString().length);
+
+// // console.log(num1.toFixed(2));
+
+// const num3=1000000;
+
+// // console.log(num3.toPrecision(2));
+
+// console.log(num3.toLocaleString('en-IN'));
+
+// const mudasir= Math;
+// console.log(Math.SQRT2);
+
+// console.log(Math.PI);
+// console.log(Math.max);
+
+console.log(Math.round((Math.random()*10)+1));
+
+const min=10;
+const max=20;
+console.log(Math.round((Math.random(min-max)*10)+min));
+
+console.log();
+
+
+
