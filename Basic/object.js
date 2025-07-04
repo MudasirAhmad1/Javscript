8c05b6fca2d61a7a883069da734a5ca66445996d
mudasir <mudasirahmad32@gmail.com>
2025-07-04 15:46:27 +0530

    function

--- Basic/object.js
+++ Basic/object.js
@@ -1,3 +1,23 @@
+
+//object creation //singleton constructor
+const tinderUser=new Object();
+tinderUser.id="123abc";
+tinderUser.name="Mudasir";
+tinderUser.isLoggedIn=false;
+console.log(tinderUser);
+
+const regularUser={
+    email:"some@gmail.com",
+    fullname:{
+        userfullname:{
+            firstname:"Mudasir",
+        }
+    }
+}
+console.log(regularUser.fullname.userfullname.firstname);
+
+
+//object merging
 const obj1={1:"A",2:"B",3:"C"};
 const obj2={3:"D",4:"E"};
 const obj3={obj1,obj2};
@@ -7,10 +27,6 @@ console.log(obj4);
 
 const obj5={...obj1,...obj2};
 console.log(obj5);
-
-
-
-
 
 //object destructuring
 const mudasir={
--- Basic/typeof.js
+++ Basic/typeof.js
@@ -1,18 +1,72 @@
-let num1= new Number(100);
-console.log(typeof num1);  //object 
+// // let num1= new Number(100);
+// // console.log(typeof num1);  //object 
 
-console.log(num1.toString());
+// // console.log(num1.toString());
 
 
-let num=100;
-console.log(typeof num); // number 
+// // let num=100;
+// // console.log(typeof num); // number 
 
-let charater= new String("Muzamil");
-console.log(charater);
+// // let charater= new String("Muzamil");
+// // console.log(charater);
 
-let bol= new Boolean(1);
-console.log(typeof bol);
+// // let bol= new Boolean(1);
+// // console.log(typeof bol);
 
  
+// let num=24;
+// let num1=45;
+// const num2=num++;
+// const num3=++num1;
+// console.log(num2,num3);
+
+// let x = 3;
+// const y = x++;
+
+// console.log(x,y);
+// const z = ++x;
+// console.log(z,x);
+
+
+
+const  num= String("  Mudasir   ");
+const num1=String("Muzamil");
+// console.log(num.indexOf('u'));
+// console.log(num.charAt('2'));
+
+console.log(num.substring(0,4));
+
+
+console.log(num.slice(1,4));
+console.log(num);
+
+// console.log(num1.splice(1,3));
+console.log(num1);
+
+console.log(num.trim());
+
+
+const url="mudasir%gmail.com";
+console.log(url.replace('.com','.net'));
+
+console.log(url.includes('mudasir'));
+
+console.log(url.split('%'));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
