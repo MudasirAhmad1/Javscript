88466e09cf58910e6b1c7bbce248bbb97e4c0397
mudasir <mudasirahmad32@gmail.com>
2025-07-04 16:39:51 +0530

    function

---
+++ Basic/Date.js
@@ -0,0 +1,20 @@
+// const date=new Date();
+// // console.log(date.toLocaleDateString());
+// // console.log(date.toLocaleTimeString());
+// // console.log(date.toDateString());
+// // console.log(date.toString());
+
+// const lastdate= new Date("10-1-2013");
+// console.log(lastdate.toLocaleDateString());
+
+const timestamp= Date.now();
+console.log(timestamp);
+const time=timestamp;
+const time1= new Date(1751626671569);
+console.log(time1.toDateString());
+
+
+
+
+
+
